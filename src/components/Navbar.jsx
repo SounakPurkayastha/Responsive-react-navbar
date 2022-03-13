b58b2945a8e6fc97f3eff2cbb4858fe8122cea12
Sounak Purkayastha <sounak.purkayastha@gmail.com>
2022-03-13 18:38:23 +0530

    Navbar reponsive

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+import "./Navbar.css";
+import { Route, Link } from "react-router-dom";
+import SearchIcon from "@mui/icons-material/Search";
+import Menu from "@mui/icons-material/Menu";
+import { useState } from "react";
+
+const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+
+  const buttonClickHandler = () => {
+    setShowLinks(!showLinks);
+    console.log(showLinks);
+  };
+
+  return (
+    <nav>
+      <div className="navbar-container">
+        <div className="logo">Logo</div>
+        <div className="nav-search">
+          <div className="searchbox-container">
+            <SearchIcon className="searchbox-icon" />
+            <input type="text" className="nav-input" />
+          </div>
+        </div>
+        <button className="nav-sidebar-button" onClick={buttonClickHandler}>
+          <Menu />
+        </button>
+        <div className={"nav-links " + (showLinks ? "" : "hidden")}>
+          <ul>
+            <Link to="/" className="nav-link">
+              <li>Home</li>
+            </Link>
+            <Link to="/shop" className="nav-link">
+              <li>Shop</li>
+            </Link>
+            <Link to="/about" className="nav-link">
+              <li>About</li>
+            </Link>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
